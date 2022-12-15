56fa501e532892949ae80de8ae55368c5d1f03ca
rubengg165 <rubengg@uma.es>
2022-12-15 12:23:22 +0100

    back changed

--- parcial-3/src/environments/environment.prod.ts
+++ parcial-3/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseURL: "https://ing-web-b3.herokuapp.com",
+  baseURL: "https://rubengg-back.herokuapp.com",
   clientId: "182737891985-808001okoua1q3me4rkooecn9qdhkhoj.apps.googleusercontent.com",
 };
