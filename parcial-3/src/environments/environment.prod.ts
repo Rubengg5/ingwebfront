1443db7fac1fc648d82673d3a8129d3784147281
rubengg165 <rubengg@uma.es>
2023-01-10 19:49:36 +0100

    setup

--- parcial-3/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  baseURL: "https://rubengg.herokuapp.com",
-  clientId: "182737891985-808001okoua1q3me4rkooecn9qdhkhoj.apps.googleusercontent.com",
-  cloudinaryApiUrl : "https://api.cloudinary.com/v1_1/dee6pfpam/upload",
-  cloudinaryUploadPreset : "ontg4fqa",
-  cloudinaryApiKey : "714814147251835"
-};
